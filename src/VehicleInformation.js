6e94d238d178a51bce251877ea0f6e0a25ce7188
Josh Freedman <joshfreedman@pm.me>
2021-01-24 10:37:36 -0500

    use custom field array functionality

--- src/VehicleInformation.js
+++ src/VehicleInformation.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import { useFieldArray, useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import BaseFormSection from './BaseFormSection';
 
 function VehicleInformation() {
     const { register } = useFormContext();
-    const { fields, append, remove } = useFieldArray({ name: 'vehicles' });
+    const [vehicleIds, setVehicleIds] = React.useState([]);
+    const [currentId, setCurrentId] = React.useState(0);
 
     function addNewVehicle() {
-        append({
-            make: '',
-            model: '',
-            year: '',
-            licenseNumber: '',
-            state: '',
-        });
+        setVehicleIds(previousVehicleIds => [...previousVehicleIds, currentId]);
+        setCurrentId(previousId => previousId + 1);
     }
 
-    function removeVehicle(index) {
-        return () => remove(index);
+    function removeVehicle(idToRemove) {
+        return () => setVehicleIds(
+            previousVehicleIds => previousVehicleIds.filter(id => id !== idToRemove)
+        );
     }
 
     return (
         <>
             {
-                fields.map((vehicle, index) => (
-                    <BaseFormSection key={vehicle.id} header={`Vehicle ${index + 1} of ${fields.length}`}>
+                vehicleIds.map((id, index) => (
+                    <BaseFormSection key={id} header={`Vehicle ${index + 1} of ${vehicleIds.length}`}>
                         <label>
                             Make<br />
-                            <input type="text" name={`vehicles[${index}].make`} ref={register()} />
+                            <input type="text" name={`vehicles[${index}].make`} ref={register} />
                         </label>
                         <label>
                             Model<br />
-                            <input type="text" name={`vehicles[${index}].model`} ref={register()} />
+                            <input type="text" name={`vehicles[${index}].model`} ref={register} />
                         </label>
                         <label>
                             Year<br />
-                            <input type="text" name={`vehicles[${index}].year`} ref={register()} />
+                            <input type="text" name={`vehicles[${index}].year`} ref={register} />
                         </label>
                         <label>
                             License Number<br />
-                            <input type="text" name={`vehicles[${index}].licenseNumber`} ref={register()} />
+                            <input type="text" name={`vehicles[${index}].licenseNumber`} ref={register} />
                         </label>
                         <label>
                             State<br />
-                            <input type="text" name={`vehicles[${index}].state`} ref={register()} />
+                            <input type="text" name={`vehicles[${index}].state`} ref={register} />
                         </label>
-                        <button type="button" onClick={removeVehicle(index)}>Delete</button>
+                        <button type="button" onClick={removeVehicle(id)}>Delete</button>
                     </BaseFormSection>
                 ))
             }
